1ee440028f5e35b697113ff2b15fdf485424b292
Eliott Le Moigne <ele-moig@c3r3s1.42lausanne.ch>
2026-06-30 11:58:50 +0200

    fixed some mistakes of the initserver loop, still ongoing

--- src/NetworkInfrastructure/serverManager.cpp
+++ src/NetworkInfrastructure/serverManager.cpp
@@ -9,10 +9,6 @@ ServerManager::~ServerManager(){
 }
 
 void ServerManager::initServers(){
-	/*
-	creation des sockets from server config file
-
-	*/
 	for (size_t i = 0; i < _configs.size(); i++){
 		for (size_t j = 0; j < _configs[i].getPort().size(); j++){
 			int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,21 +19,29 @@ void ServerManager::initServers(){
 			int opt = 1;
 			if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0){
 				std::cerr << "Fatal: setsockopt failed." << std::endl;
+				close(listen_fd);
 				continue;
 			}
 			if (fcntl(listen_fd, F_SETFL, O_NONBLOCK) < 0){
 				std::cerr << "Fatal: fcntl failed." << std::endl;
+				close(listen_fd);
 				continue;
 			}
 			struct sockaddr_in serv_addr;
-			bzero((char*) &serv_addr, sizeof(serv_addr));
+			bzero((char*)&serv_addr, sizeof(serv_addr));
 			serv_addr.sin_family = AF_INET;
 			serv_addr.sin_addr.s_addr = INADDR_ANY;
-			serv_addr.sin_port = htons(_configs[i].getPort(i));
+			serv_addr.sin_port = htons(_configs[i].getPort(j));
 			if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 				std::cerr << "Fatal: Bind failed." << std::endl;
+				close(listen_fd);
+				continue;//should have a return value for error and stop the loop
 			}
-			listen(listen_fd, 5);
+			if (listen(listen_fd, 5) < 0){
+				std::cerr << "Fatal: Listen failed." << std::endl;
+				close(listen_fd);
+				continue;
+			}
 			pollfd new_poll;
 			new_poll.fd = listen_fd;
 			new_poll.events = POLLIN;
