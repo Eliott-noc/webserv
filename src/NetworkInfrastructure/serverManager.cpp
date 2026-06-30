65a54f57863462ad0d58143bc715d7cb6954fe89
Eliott Le Moigne <ele-moig@c3r3s1.42lausanne.ch>
2026-06-30 12:36:12 +0200

    fixed and cleared initServers"

--- src/NetworkInfrastructure/serverManager.cpp
+++ src/NetworkInfrastructure/serverManager.cpp
@@ -5,6 +5,10 @@ ServerManager::ServerManager(std::vector<ServerConfig> configs){
 }
 
 ServerManager::~ServerManager(){
+	initServers();
+	if (_pollfds.size() == 0){
+		std::cerr << "Fatal: "
+	}
 
 }
 
@@ -12,21 +16,23 @@ void ServerManager::initServers(){
 	for (size_t i = 0; i < _configs.size(); i++){
 		int err_code, port;
 		for (size_t j = 0; j < _configs[i].getPort().size(); j++){
+			port = _configs[i].getPort(j);
 			int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 			if (listen_fd < 0){
 				err_code = errno;
-				printPortErr(err_code, _configs[i].getPort(j));
+				printPortErr(err_code, port);
 				continue;
 			}
 			int opt = 1;
 			if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0){
-				std::cerr << "Fatal: setsockopt failed." << std::endl;
+				err_code = errno;
+				printPortErr(err_code, port);
 				close(listen_fd);
 				continue;
 			}
 			if (fcntl(listen_fd, F_SETFL, O_NONBLOCK) < 0){
 				err_code = errno;
-				printPortErr(err_code, _configs[i].getPort(j));
+				printPortErr(err_code, port);
 				close(listen_fd);
 				continue;
 			}
@@ -34,16 +40,16 @@ void ServerManager::initServers(){
 			bzero((char*)&serv_addr, sizeof(serv_addr));
 			serv_addr.sin_family = AF_INET;
 			serv_addr.sin_addr.s_addr = INADDR_ANY;
-			serv_addr.sin_port = htons(_configs[i].getPort(j));
+			serv_addr.sin_port = htons(port);
 			if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 				err_code = errno;
-				printPortErr(err_code, _configs[i].getPort(j));
+				printPortErr(err_code, port);
 				close(listen_fd);
 				continue;
 			}
 			if (listen(listen_fd, 5) < 0){
 				err_code = errno;
-				printPortErr(err_code, _configs[i].getPort(j));
+				printPortErr(err_code, port);
 				close(listen_fd);
 				continue;
 			}
