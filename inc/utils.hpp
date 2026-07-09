51a9a14538e7f96cb2fd8d6d06be1f34a21f9e80
William Hollebeque <whollebe@c2r2s1.42lausanne.ch>
2026-07-09 15:12:10 +0200

    checkLocation de julien fini

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -3,9 +3,10 @@
 
 #include "locArgs.hpp"
 
-int	checkDuplicateIndex(const std::vector<std::string> &args);
-int	checkDuplicateMethods(const std::string &arg, t_methods *methods);
-int	isServKeyword(const std::string &str);
-int	isLocKeyword(const std::string &str);
+int		checkDuplicateIndex(const std::vector<std::string> &args);
+int		checkDuplicateMethods(const std::string &arg, t_methods *methods);
+int		isServKeyword(const std::string &str);
+int		isLocKeyword(const std::string &str);
+bool	checkLocation(const Location &Location);
 
 #endif
